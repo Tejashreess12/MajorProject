7214a6038a3264799ea95013b87080f4edd86c45
Tejashreess12 <66642088+Tejashreess12@users.noreply.github.com>
2021-02-05 20:39:21 +0530

    Update module3.py

--- module3.py
+++ module3.py
@@ -3,6 +3,7 @@ import math
 
 
 def detect_hand(frame, binary):
+    roi = frame[100:450, 250:600]
     contours, _ = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     palm_area = 0
     flag = None
@@ -16,9 +17,9 @@ def detect_hand(frame, binary):
     if flag is not None and palm_area > 10000:
         cnt = contours[flag]
         contours = cnt
-        cv2.drawContours(frame, [cnt], 0, (0, 255, 0), 2)
+        cv2.drawContours(roi, [cnt], 0, (0, 255, 0), 2)
         if len(cnt) == 0:
-            return frame
+            return frame, [(0, 0)], (0, 0)
         else:
             points = []
             hull = cv2.convexHull(cnt, returnPoints=False)
@@ -38,20 +39,20 @@ def detect_hand(frame, binary):
 
             fingertips = [pt for idx, pt in zip(range(5), filtered)]
             for fingertip in fingertips:
-                cv2.circle(frame, fingertip, 5, (0, 0, 255), -1)
+                cv2.circle(roi, fingertip, 5, (0, 0, 255), -1)
 
             if len(contours) == 0:
-                pass
+                return frame, [(0, 0)], (0, 0)
             else:
                 M = cv2.moments(contours)
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
                 com = (cX, cY)
-                cv2.circle(frame, com, 10, (255, 0, 0), -1)
-            return frame
+                cv2.circle(roi, com, 10, (255, 0, 0), -1)
+            return frame, fingertips, com
     else:
-        return frame
+        return frame, [(0, 0)], (0, 0)
+
 
 def dist(a, b):
     return math.sqrt((a[0] - b[0]) ** 2 + (b[1] - a[1]) ** 2)
-
