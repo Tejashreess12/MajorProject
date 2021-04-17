a0fe6a3580deca312bf7a585316231f6ea0fb5a8
Tejashreess12 <66642088+Tejashreess12@users.noreply.github.com>
2021-04-17 20:30:10 +0530

    Create module5.py

---
+++ module5.py
@@ -0,0 +1,34 @@
+from pynput.mouse import Button, Controller
+import pyautogui
+import module3
+import cv2
+
+
+def assign(frame, count_defects,fingertips,com):
+    mouse = Controller()
+    if count_defects == 0:
+        distance = module3.dist(fingertips[0], com)
+        if distance < 150:
+            cv2.putText(frame, '0', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+            mouse.click(Button.left, 2)
+        elif distance < 200:
+            cv2.putText(frame, 'Best of luck', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+        else:
+            cv2.putText(frame, "ONE", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+            mouse.click(Button.left, 1)
+    elif count_defects == 1:
+        cv2.putText(frame, "TWO", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+        # pyautogui.moveRel(25, 0)
+    elif count_defects == 2:
+        cv2.putText(frame, "THREE", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+        # pyautogui.moveRel(0, 25)
+    elif count_defects == 3:
+        cv2.putText(frame, "FOUR", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+        # pyautogui.moveRel(-25, 0)
+    elif count_defects == 4:
+        cv2.putText(frame, "FIVE", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+        # pyautogui.moveRel(0, -25)
+    else:
+        pass
+
+    return frame
