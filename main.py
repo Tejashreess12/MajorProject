d48e5591d09392b685dfce4283a99fc2bb9c0110
Tejashreess12 <66642088+Tejashreess12@users.noreply.github.com>
2021-04-17 20:43:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,32 +22,7 @@ while True:
     frame, fingertips, com = module3.detect_hand(frame, detected_hand)
     count_defects = module4.recognise(detected_hand)
     module5.assign(frame,count_defects,fingertips,com)
-    '''
-    if count_defects == 0:
-        distance = module3.dist(fingertips[0], com)
-        if distance < 150:
-            cv2.putText(frame, '0', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-            #mouse.click(Button.left, 2)
-        elif distance < 200:
-            cv2.putText(frame, 'Best of luck', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-        else:
-            cv2.putText(frame, "ONE", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-            #mouse.click(Button.left, 1)
-    elif count_defects == 1:
-        cv2.putText(frame, "TWO", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-        #pyautogui.moveRel(25, 0)
-    elif count_defects == 2:
-        cv2.putText(frame, "THREE", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-        #pyautogui.moveRel(0, 25)
-    elif count_defects == 3:
-        cv2.putText(frame, "FOUR", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-        #pyautogui.moveRel(-25, 0)
-    elif count_defects == 4:
-        cv2.putText(frame, "FIVE", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-        #pyautogui.moveRel(0, -25)
-    else:
-        pass
-'''
+   
     cv2.imshow("Hand", frame)
     k = cv2.waitKey(5)
     if k == ord('q'):
