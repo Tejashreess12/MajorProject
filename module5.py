5583a7b26ce3e2451173cb2fc58cb0450d439cfb
Tejashreess12 <66642088+Tejashreess12@users.noreply.github.com>
2021-04-26 16:48:13 +0530

    Update module5.py

--- module5.py
+++ module5.py
@@ -10,25 +10,26 @@ def assign(frame, count_defects,fingertips,com):
         distance = module3.dist(fingertips[0], com)
         if distance < 150:
             cv2.putText(frame, '0', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+
         elif distance < 200:
-            cv2.putText(frame, 'Best of luck', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+            cv2.putText(frame, 'Double click', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
             mouse.click(Button.left, 2)
-            time.sleep(0.5)         
+            time.sleep(0.5)
         else:
-            cv2.putText(frame, "ONE", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+            cv2.putText(frame, "Single Click", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
             mouse.click(Button.left, 1)
             time.sleep(0.5)
     elif count_defects == 1:
-        cv2.putText(frame, "TWO", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+        cv2.putText(frame, "Right", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
         pyautogui.moveRel(25, 0)
     elif count_defects == 2:
-        cv2.putText(frame, "THREE", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+        cv2.putText(frame, "Down", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
         pyautogui.moveRel(0, 25)
     elif count_defects == 3:
-        cv2.putText(frame, "FOUR", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+        cv2.putText(frame, "Left", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
         pyautogui.moveRel(-25, 0)
     elif count_defects == 4:
-        cv2.putText(frame, "FIVE", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+        cv2.putText(frame, "Up", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
         pyautogui.moveRel(0, -25)
     else:
         pass
