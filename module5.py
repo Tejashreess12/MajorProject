01207529d87533808bdb2a0f598ddca6e05ff340
Tejashreess12 <66642088+Tejashreess12@users.noreply.github.com>
2021-05-21 16:34:47 +0530

    Update module5.py

--- module5.py
+++ module5.py
@@ -12,25 +12,25 @@ def assign(frame, count_defects,fingertips,com):
             cv2.putText(frame, '0', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
 
         elif distance < 200:
-            cv2.putText(frame, 'Double click', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+            cv2.putText(frame, 'Double Click', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
             mouse.click(Button.left, 2)
             time.sleep(0.5)
         else:
-            cv2.putText(frame, "Single Click", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+            cv2.putText(frame, "Click", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
             mouse.click(Button.left, 1)
             time.sleep(0.5)
     elif count_defects == 1:
         cv2.putText(frame, "Right", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-        pyautogui.moveRel(25, 0)
+        pyautogui.moveRel(20, 0)
     elif count_defects == 2:
         cv2.putText(frame, "Down", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-        pyautogui.moveRel(0, 25)
+        pyautogui.moveRel(0, 20)
     elif count_defects == 3:
         cv2.putText(frame, "Left", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-        pyautogui.moveRel(-25, 0)
+        pyautogui.moveRel(-20, 0)
     elif count_defects == 4:
         cv2.putText(frame, "Up", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-        pyautogui.moveRel(0, -25)
+        pyautogui.moveRel(0, -20)
     else:
         pass
 
